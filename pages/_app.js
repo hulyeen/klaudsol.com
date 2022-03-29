ab4005aff95ee1100eefbf56396d07cef18a1733
Ardee Aram <ardee.aram@safelinkhub.com>
2022-03-29 08:49:59 +0000

    Make legacy scripts work with react

--- pages/_app.js
+++ pages/_app.js
@@ -2,23 +2,18 @@ import '../styles/globals.css';
 import "@/public/assets/css/style.css"
 import Script from 'next/script';
 import $ from "jquery";
+import runLegacyScripts from '@/components/legacy';
 import { useEffect }  from 'react';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
+    runLegacyScripts($);
   }, []);
   return (
       <>
         <Component {...pageProps} />
-          {/* Latest jQuery */}
-          {/*<Script src="/assets/js/jquery.min.js" strategy="beforeInteractive"></Script>*/}
           {/* Plugin js */}
           {/*<Script src="/assets/js/plugin.js"></Script>*/}
-          {/*<Script src="/assets/js/jquery.mixitup.js"></Script>*/}
-          {/* GOOGLE MAP JS */}
-          {/*<Script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIvoZGVhG5uh6ZkXGFPrYLrJ939mfbX2Q"></Script>*/}
-          {/* Main js*/}
-          {/*<Script src="/assets/js/main_script.js" strategy="lazyOnload"></Script>*/}
       </>
     )
 }
